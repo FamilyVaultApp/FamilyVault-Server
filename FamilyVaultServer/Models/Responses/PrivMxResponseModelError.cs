cc9208721fbd5369399a63643e766e9fbd7c95b7
adama789 <cedrq@tlen.pl>
2025-02-02 11:57:22 +0100

    :)

--- FamilyVaultServer/Models/Responses/PrivMxResponseModelError.cs
+++ FamilyVaultServer/Models/Responses/PrivMxResponseModelError.cs
@@ -1,9 +1,14 @@
-﻿namespace FamilyVaultServer.Models.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace FamilyVaultServer.Models.Responses
 {
     public class PrivMxResponseModelError
     {
+        [JsonPropertyName("code")]
         public int Code { get; set; }
+        [JsonPropertyName("message")]
         public string Message { get; set; } = string.Empty;
+        [JsonPropertyName("data")]
         public object? Data { get; set; }
     }
 }
