6893ddd48e7d12b3e190207f24c5b9e8ccafe00a
Adam Cedro <cedrq@tlen.pl>
2025-04-05 17:55:22 +0200

    AddGetFamilyGroupNameEndpoint

--- FamilyVaultServer/Models/Requests/GetFamilyGroupNameRequest.cs
+++ FamilyVaultServer/Models/Requests/GetFamilyGroupNameRequest.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace FamilyVaultServer.Models.Requests
 {
-    public class GetFamilyGroupInformationRequest
+    public class GetFamilyGroupNameRequest
     {
         [Required]
         [JsonPropertyName("contextId")]
