f91aed88036413643c38cd42b0be78371c6022c7
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-04-16 14:02:57 +0200

    Implement test ACL permissions

--- FamilyVaultServer/Utils/PermissionGroupToAclMapper.cs
+++ FamilyVaultServer/Utils/PermissionGroupToAclMapper.cs
@@ -9,8 +9,30 @@ namespace FamilyVaultServer.Utils
             return permissionGroup switch
             {
                 PermissionGroup.Guardian => "ALLOW ALL",
-                PermissionGroup.Member => "ALLOW ALL",
-                PermissionGroup.Guest => "ALLOW ALL",
+                PermissionGroup.Member =>
+                    "ALLOW thread/READ\n" +
+                    "ALLOW thread/threadCreate\n" +
+                    "ALLOW thread/threadUpdate\n" +
+                    "ALLOW thread/threadMessageSend\n" +
+                    "ALLOW thread/threadMessageDelete\n" +
+                    "ALLOW thread/threadDelete\n" +
+                    "ALLOW store/READ\n" +
+                    "ALLOW store/storeFileCreate\n" +
+                    "ALLOW store/storeFileWrite\n" +
+                    "ALLOW store/storeFileDelete\n" +
+                    "ALLOW inbox/READ\n" +
+                    "ALLOW stream/READ",
+
+                PermissionGroup.Guest =>
+                    "ALLOW thread/READ\n" +
+                    "ALLOW thread/threadMessageSend\n" +
+                    "ALLOW thread/threadMessageDelete\n" +
+                    "ALLOW store/READ\n" +
+                    "ALLOW store/storeFileCreate\n" +
+                    "ALLOW store/storeFileDelete\n" +
+                    "ALLOW inbox/READ\n" +
+                    "ALLOW stream/READ",
+
                 _ => throw new ArgumentException("Provided not valid PermissionGroup"),
             };
         }
