31a3ba16ee7ec363802683668ae39ddb8612dd25
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-05-10 19:32:59 +0200

    Add GetBridgeUrl endpoint

--- FamilyVaultServer/Controllers/ApplicationConfigController.cs
+++ FamilyVaultServer/Controllers/ApplicationConfigController.cs
@@ -10,16 +10,24 @@ namespace FamilyVaultServer.Controllers
     public class ApplicationConfigController : ControllerBase
     {
         private readonly IPrivMxService _privMxService;
+        private readonly string _bridgeUrl;
 
         public ApplicationConfigController(IOptions<PrivMxOptions> privMxOptions, IPrivMxService privMxService)
         {
             _privMxService = privMxService;
+            _bridgeUrl = privMxOptions.Value.Url;
         }
 
         [HttpGet]
         public async Task<ActionResult<GetSolutionIdResponse>> GetSolutionId()
         {
             return Ok(new GetSolutionIdResponse { SolutionId = await _privMxService.GetSolutionId() });
+        }
+
+        [HttpGet]
+        public ActionResult<GetBridgeUrlResponse> GetBridgeUrl()
+        {
+            return Ok(new GetBridgeUrlResponse { BridgeUrl = _bridgeUrl });
         }
     }
 }
